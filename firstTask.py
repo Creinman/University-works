7955589c2163e7b40085f182a9ba466382ffcc01
Creinman <68871139+Creinman@users.noreply.github.com>
2020-09-19 23:49:15 +0300

    new commit

--- firstTask.py
+++ firstTask.py
@@ -5,9 +5,7 @@ plt.ioff()
 x = np.arange(-5.12, 5.12)
 y = 10 + x**2 - 10*np.cos(2*np.pi*x)
 fig, ax = plt.subplots()
- 
 ax.plot(x, y)  
 lgnd = ax.legend(['y'], loc='upper center', shadow=True)
 lgnd.get_frame().set_facecolor('green')
- 
 plt.show()
