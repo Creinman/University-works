1029e3cc2fa03671762d7ff0c394093886af7837
Oleg <sosolala404@gmail.com>
2020-09-19 23:27:16 +0300

    Add first tast (var 1)

---
+++ firstTask.py
@@ -0,0 +1,13 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+
+plt.ioff()
+x = np.arange(-10, 10)
+y = 10 + x**2 + 10*np.cos(2*np.pi*x)
+fig, ax = plt.subplots()
+ 
+ax.plot(x, y)  
+lgnd = ax.legend(['y'], loc='upper center', shadow=True)
+lgnd.get_frame().set_facecolor('green')
+ 
+plt.show()
