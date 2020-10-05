50a7c1beadf4b189dc61f74fa4c99fe6a6668c26
Creinman <68871139+Creinman@users.noreply.github.com>
2020-10-05 23:42:40 +0300

    Update firstTask.py

--- firstTask.py
+++ firstTask.py
@@ -1,8 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.ioff()
-x = np.arange(-5.12, 5.12)
+x = np.arange(-5.12, 5.12, 0.1)
 y = 10 + x**2 - 10*np.cos(2*np.pi*x)
 fig, ax = plt.subplots()
 ax.plot(x, y)  
